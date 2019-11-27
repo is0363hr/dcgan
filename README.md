4b22c4e904ca7e99990c0531501544900f5c0ca4
tamakawa <47349342+is0363hr@users.noreply.github.com>
2019-11-27 17:12:12 +0900

    test

--- README.md
+++ README.md
@@ -1,20 +1,8 @@
-# 開発環境
+Overview
 
-| 使用ツール | バージョン | URL |
-| ---- | ---- | ---- |
-| python(仮想環境) | 2.7 | https://www.anaconda.com/ |
-| tensorflow | 1.10.0 | |
+DCGANを動かしてみた。
 
-## 使用ライブラリ (Python)
-| 使用ライブラリ | 説明 |
-| ---- | ---- |
-| keras | ニューラルネットワークライブラリ |
-| Numpy | python用の数値計算Wrapperライブラリ |
-| requests | |
-| beautifulsoup4 | |
-| lxml | |
-
-# 環境構築
+## Description
 
 以下、動作確認済みの環境構築を説明する。
 
@@ -22,6 +10,89 @@ pythonのバージョンは2.7だと動いた。
 3.6だと動かない説。
 
 Anacondaやpyenvの仮想環境を使用するのがオススメ。
+
+Anacondaで仮想環境を作成したため、以下condaコマンドを使用する。
+pipでも可。
+conda と pip の混在はダメ絶対。
+
+## Requirement
+
+### 開発環境
+
+| 使用ツール | バージョン | URL |
+| ---- | ---- | ---- |
+| python(仮想環境) | 2.7 | https://www.anaconda.com/ |
+| tensorflow | 1.10.0 | http://tensorflow.classcat.com/2018/08/10/tensorflow-1-10-0-release-note/ |
+
+### 使用ライブラリ (Python)
+
+| 使用ライブラリ | 説明 |
+| ---- | ---- |
+| keras | ニューラルネットワークライブラリ |
+| Numpy | python用の数値計算Wrapperライブラリ |
+| scipy | 高度な科学計算を行うためのライブラリ |
+| requests | httpライブラリ |
+| beautifulsoup4 | Webページから情報を収集できるライブラリ |
+| lxml | libxml2/libxsltとElementTree APIを組み合わせた強力でPythonicなXML処理ライブラリ |
+
+## Install & Usage
+
+### DCGAN
+
+まず、クローンしてソースコードをダウンロードしよう。
+
+```bash
+git clone https://github.com/is0363hr/dcgan.git
+cd gan
+python main.py
+```
+
+たぶんそのままじゃ動かないので以下環境構築の説明。
+
+上記で説明したライブラリをインストールする。
+ただし、スクレイピングで画像を収集する必要がないなら、requests以下のライブラリは必要ない。
+
+```bash
+conda install tensorflow=1.10.0
+conda install keras
+conda install scipy
+```
+
+OpenCVは安定したバージョンじゃないと動かなかったので注意
+
+```bash
+conda install opencv
+```
+
+
+### スクレイピング
+
+画像をネットから自動で収集したい人は必見！
+スクレイピングで簡単に画像を収集しよう。
+でも、yahooの画像サイトとかはimgタグで画像が張られてないから取れないいいいいい
+
+```bash
+conda install requests
+conda install beautifulsoup4
+conda install lxml
+```
+
+
+## Demo
+
+## VS.
+
+## Contribution
+
+## Licence
+
+## References
+
+
+
+## Author
+
+[tamakawa](https://github.com/is0363hr)
 
 
 
--- gan/collect.py
+++ gan/collect.py
@@ -1,22 +1,133 @@
-# scraping-img-1.py
+# coding: utf-8
 
-import requests # urlを読み込むためrequestsをインポート
-from bs4 import BeautifulSoup # htmlを読み込むためBeautifulSoupをインポート
+import os
+import sys
+import traceback
+from mimetypes import guess_extension
+from time import time, sleep
+from urllib.request import urlopen, Request
+from urllib.parse import quote
+from bs4 import BeautifulSoup
 
-URL = 'https://su-gi-rx.com'  # URL入力
-images = []  # 画像リストの配列
+MY_EMAIL_ADDR = ''
 
-soup = BeautifulSoup(requests.get(URL).content, 'lxml')  # bsでURL内を解析
+class Fetcher:
+    def __init__(self, ua=''):
+        self.ua = ua
 
-for link in soup.find_all("img"):  # imgタグを取得しlinkに格納
-    if link.get("src").endswith(".jpg"):  # imgタグ内の.jpgであるsrcタグを取得
-        images.append(link.get("src"))  # imagesリストに格納
-    elif link.get("src").endswith(".png"):  # imgタグ内の.pngであるsrcタグを取得
-        images.append(link.get("src"))  # imagesリストに格納
+    def fetch_img_direct(self, url):
+        """
+        yahoo画像検索画面に表示されている画像のbyte情報を抽出します。
+        引数:
+            url: yahoo画像検索画面のurlです。
 
-for target in images:  # imagesからtargetに入れる
-    re = requests.get(target)
-    with open('img/' + target.split('/')[-1], 'wb') as f:  # imgフォルダに格納
-        f.write(re.content)  # .contentにて画像データとして書き込む
+        返り値:
+            img_b_content: ウェブサイトリソースのbyteコードのリストです。
+            mime: CONTENT_TYPEにより指定されている拡張子です。
+        """
+        req = Request(url, headers={'User-Agent': self.ua})
+        try:
+            with urlopen(req, timeout=3) as p:
+                page_b_content = p.read()
+                structured_page = BeautifulSoup(page_b_content.decode('UTF-8'), 'html.parser')
+                img_link_elems = structured_page.find_all('img')
+                img_urls = [e.get('src') for e in img_link_elems if e.get('src').startswith('http')]
+                img_urls = list(set(img_urls)) #なぜset化しているのかは不明
+        except:
+            sys.stderr.write('Error in fetching {}\n'.format(url))
+            sys.stderr.write(traceback.format_exc())
+            return None, None
 
-print("ok") # 確認
+        img_b_content = []
+        mime = []
+        for i, img_url in enumerate(img_urls):
+            req1 = Request(img_url, headers={'User-Agent': self.ua})
+            try:
+                with urlopen(req1, timeout=3) as p:
+                    img_b_content.append(p.read())
+                    mime.append(p.getheader('Content-Type'))
+            except:
+                sys.stderr.write('Error in fetching {}\n'.format(img_url))
+                sys.stderr.write(traceback.format_exc())
+                continue
+
+        return img_b_content, mime
+
+fetcher = Fetcher(MY_EMAIL_ADDR)
+
+
+
+def url_brancher(word):
+    """
+    yahoo画像検索画面のurlを、検索条件の組み合わせの数だけ取得します。
+
+    引数:
+        word : 検索語です。
+
+    返り値:
+        urllist : yahoo画像検索画面のurlのリストです。
+    """
+    constant = "https://search.yahoo.co.jp/image/search?p={}&n=60".format(quote(word))
+
+    values = [\
+    ["", "small"],\
+    ["", "red"],\
+    ["", "face"]\
+    ]
+    """
+    values = [\
+    ["", "small", "medium", "large", "wallpaper", "widewallpaper"],\
+    ["", "red", "orange", "yellow", "green", "teal", "blue", "purple", "pink", "white", "gray", "black", "brown"],\
+    ["", "face", "photo", "clipart", "lineart"]\
+    ]
+    """
+    urllist = []
+
+    for i in range(len(values[0])):
+        for j in range(len(values[1])):
+            for k in range(len(values[2])):
+                urllist.append(constant + "&dim={}".format(values[0][i]) + "&imc={}".format(values[1][j]) + "&ctype={}".format(values[2][k]))
+    return urllist
+
+
+
+def main(word):
+    """
+    Fetchにより取得された情報をもとに画像ファイルを保存します。
+
+    引数:
+    word: 検索語です。
+
+    返り値:
+    """
+    data_dir = 'data/'
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    yahoo_url_list = url_brancher(word)
+
+    for i, yahoo_url in enumerate(yahoo_url_list):
+        sleep(0.1)
+        img, mime = fetcher.fetch_img_direct(yahoo_url)
+        if not mime or not img:
+            print('Error in fetching {}\n'.format(yahoo_url))
+            continue
+
+        for j, img_url in enumerate(img):
+            ext = guess_extension(mime[j].split(';')[0])
+            if ext in ('.jpe', '.jpeg'):
+                ext = '.jpg'
+            if not ext:
+                print('Error in fetching {}\n'.format(img_url))
+                continue
+
+            result_file = os.path.join(data_dir, str(i) + str(j)+ ext)
+            with open(result_file, mode='wb') as f:
+                f.write(img_url)
+            print('fetched', str(i) + str(j) + ext)
+
+
+
+if __name__ == '__main__':
+    word = input("検索ワードを入力してください : ")
+    main(word)
